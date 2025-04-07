50e468f70728bfadffcb0d9214dc4642104d6812
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-04-07 23:16:30 +0400

    run script

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 javac com/craftinginterpreters/tool/*.java && ^
 java  com.craftinginterpreters.tool.GenerateAst com\craftinginterpreters\lox && ^
 javac com/craftinginterpreters/lox/*.java && ^
-java  com.craftinginterpreters.lox.%1
+java  com.craftinginterpreters.lox.%1 %2
 
