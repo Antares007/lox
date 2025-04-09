91907f4a7b4e959794ac3253012344e15e58021a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-04-09 12:46:52 +0400

    Instance

--- com/craftinginterpreters/lox/LoxClass.java
+++ com/craftinginterpreters/lox/LoxClass.java
@@ -1,7 +1,7 @@
 package com.craftinginterpreters.lox;
 import java.util.List;
 import java.util.Map;
-class LoxClass {
+class LoxClass implements LoxCallable  {
   final String name;
   LoxClass(String name) {
     this.name = name;
@@ -9,5 +9,14 @@ class LoxClass {
   @Override
   public String toString() {
     return name;
+  }
+  @Override
+  public Object call(Interpreter interpreter, List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+    return instance;
+  }
+  @Override
+  public int arity() {
+    return 0;
   }
 }
