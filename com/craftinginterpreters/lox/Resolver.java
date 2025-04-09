1bd735bdacc508a265d744da51079f2007bb0e3f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-04-09 15:38:10 +0400

    init

--- com/craftinginterpreters/lox/Resolver.java
+++ com/craftinginterpreters/lox/Resolver.java
@@ -16,8 +16,18 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     FUNCTION,
     METHOD
   }
+  private enum ClassType {
+    NONE,
+    CLASS
+  }
+  private ClassType currentClass = ClassType.NONE;
   @Override
   public Void visitThisExpr(Expr.This expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword,
+          "Can't use 'this' outside of a class.");
+      return null;
+    }
     resolveLocal(expr, expr.keyword);
     return null;
   }
@@ -34,6 +44,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   }
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
+    ClassType enclosingClass = currentClass;
+    currentClass = ClassType.CLASS;
     declare(stmt.name);
     define(stmt.name);
     beginScope();
@@ -43,6 +55,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
       resolveFunction(method, declaration);
     }
     endScope();
+    currentClass = enclosingClass;
     return null;
   }
   @Override
