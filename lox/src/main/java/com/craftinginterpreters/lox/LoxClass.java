30b02f6d99aeb48cd83dc69b58d313e1305be08a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-04-14 17:36:36 +0400

    Inheritance

--- lox/src/main/java/com/craftinginterpreters/lox/LoxClass.java
+++ lox/src/main/java/com/craftinginterpreters/lox/LoxClass.java
@@ -34,6 +34,9 @@ class LoxClass implements LoxCallable  {
     if (methods.containsKey(name)) {
       return methods.get(name);
     }
+    if (superclass != null) {
+      return superclass.findMethod(name);
+    }
     return null;
   }
 }
